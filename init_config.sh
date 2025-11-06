c2e37c64123eaa8a13b49e23ae7758c79448e12f
Dan Evans <danev1@gmail.com>
2025-11-05 21:31:15 -0800

    file tests

--- init_config.sh
+++ init_config.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-#create bin dir in home to contain binaries
-mkdir $HOME/bin
-mkdir $HOME/results
+#create bin and results dirs in home
+if [ -d "$HOME/bin" ]; then
+  echo "$HOME/bin exists"
+else
+  mkdir $HOME/bin
+fi
+
+if [ -d "$HOME/results" ]; then
+  echo "$HOME/results exists"
+else
+  mkdir $HOME/results
+fi
 
 # move existing bash profile to backup, copy new profile to home
 mv ../.bash_profile ../.bash_profile_bak

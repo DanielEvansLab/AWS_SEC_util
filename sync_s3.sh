8b00d252d2511ad9da463d2eae272938b25952fb
Dan Evans <danev1@gmail.com>
2025-11-05 21:21:26 -0800

    file exist tests

--- sync_s3.sh
+++ sync_s3.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-mkdir $HOME/data
+if [ -d "$HOME/data" ]; then
+  echo "$HOME/data exists"
+else
+  mkdir $HOME/data
+fi
 
 aws s3 sync s3://alliston-evans-181-4-c-us-west-2.sec.ucsf.edu/OAI_pheno/ $HOME/data
 
-mkdir $HOME/gwas_data
+if [ -d "$HOME/gwas_data" ]; then
+  echo "$HOME/gwas_data exists"
+else
+  mkdir $HOME/gwas_data
+fi
 
 aws s3 sync s3://alliston-evans-181-4-c-us-west-2.sec.ucsf.edu/OAI_topmed_data/ $HOME/gwas_data
 

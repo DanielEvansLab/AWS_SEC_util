4f0c14c17b29cb681324bcf9aacee79de3e4aba2
Dan Evans <danev1@gmail.com>
2025-11-05 21:00:35 -0800

    more plink cmds

--- plink_run.sh
+++ plink_run.sh
@@ -6,5 +6,15 @@ date
 chrs=({1..22})
 
 for chr in "${chrs[@]}"; do
-	echo "this is chromosome ${chr}"
+  echo "Analyzing chromosome ${chr}"
+  plink2 --pfile $HOME/gwas_data/chr${chr} \
+  --pheno iid-only $HOME/data/pheno.txt \
+  --no-psam-pheno \
+  --covar iid-only $HOME/data/covar.txt \
+  --no-input-missing-phenotype \
+  --covar-variance-standardize \
+  --glm hide-covar \
+  --out $HOME/results/chr${chr}
 done 
+
+date

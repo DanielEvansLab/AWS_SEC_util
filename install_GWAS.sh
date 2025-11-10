a0c80b49d1a334ee47a15413bdba4bba003ba05f
Dan Evans <danev1@gmail.com>
2025-11-10 13:25:46 -0800

    plink1.9 install

--- install_GWAS.sh
+++ install_GWAS.sh
@@ -5,5 +5,6 @@ cd $HOME/bin
 wget https://s3.amazonaws.com/plink2-assets/plink2_linux_avx2_20251026.zip
 unzip plink2*.zip
 
-
+wget https://s3.amazonaws.com/plink1-assets/plink_linux_x86_64_20250819.zip
+unzip plink*.zip
 

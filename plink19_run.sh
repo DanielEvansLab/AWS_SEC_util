a72b4054dbbd99ea37a404114cf5ed11c9d54541
Dan Evans <danev1@gmail.com>
2025-12-22 14:05:50 -0800

    zout

--- plink19_run.sh
+++ plink19_run.sh
@@ -5,19 +5,23 @@ hostname
 date
 
 chrs=({1..22})
+phenos=(hip_fem_l hip_fem_r)
+
+for pheno in "${phenos[@]}"; do
 for chr in "${chrs[@]}"; do
-  echo "****************************************"
-  echo "*****Analyzing chromosome ${chr}********"
-  echo "****************************************"
+  echo "***********************************************************"
+  echo "*****Analyzing phenotype ${pheno} chromosome ${chr}********"
+  echo "***********************************************************"
   plink --pheno $HOME/data/mrospheno.txt \
-  --all-pheno \
+  --pheno-name ${pheno} \
   --covar $HOME/data/mroscov.txt \
   --allow-no-sex \
   --fam $HOME/gwas_data/MrOS/chr${chr}.plink.fam \
   --map $HOME/gwas_data/MrOS/chr${chr}.plink.map \
-  --dosage $HOME/gwas_data/MrOS/chr${chr}.plink.dose.gz noheader format=1 \
-  --out $HOME/results/chr${chr}
+  --dosage $HOME/gwas_data/MrOS/chr${chr}.plink.dose.gz noheader format=1 Zout\
+  --out $HOME/results/${pheno}_chr${chr}
 
+done
 done
 
 date
